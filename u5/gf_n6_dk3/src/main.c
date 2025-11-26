79f48eee195c6880afba2dcab1c08efb666f7808
Roman Pustobaiev <romanpustobayev@gmail.com>
2025-11-26 12:41:37 +0100

    More tweaks to add stm32n6 support

--- u5/gf_n6_dk3/src/main.c
+++ u5/gf_n6_dk3/src/main.c
@@ -4,11 +4,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/kernel.h>
 #include <stdio.h>
+
+// #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+// #include <zephyr/logging/log.h>
+// LOG_MODULE_REGISTER(main);
+
+// static const struct device *video_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_camera));
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
-
+	// if (!device_is_ready(video_dev)) {
+	// 	LOG_ERR("%s device is not ready", video_dev->name);
+	// }
+	// LOG_DBG("%s", video_dev->name);
 	return 0;
 }
